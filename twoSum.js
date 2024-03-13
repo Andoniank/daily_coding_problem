d437c7a0f9700a37f54f41b3828ce0d6e83836d1
Andoniank <andoniank@gmail.com>
2024-03-12 23:44:38 -0700

    Add twoSum problem with test cases

---
+++ twoSum.js
@@ -0,0 +1,12 @@
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+const twoSum = function (array, sum) {
+    
+}
+
+console.log(twoSum([10, 15, 3, 7], 17))                // true
+console.log(twoSum([1, 3, 7, 10], 9))                  // false
+console.log(twoSum([50, 9, 36, 71, 16, 42, 19], 58))   // true
+console.log(twoSum([1, 2, 3], 5))                      // true
+console.log(twoSum([1, 3], 6))                         // false
