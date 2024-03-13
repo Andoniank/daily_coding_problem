82cac43268f351cf89091063c5a12c4b91a78661
Andoniank <andoniank@gmail.com>
2024-03-12 23:55:07 -0700

    Add solution

--- twoSum.js
+++ twoSum.js
@@ -1,8 +1,17 @@
+// 3-12-2024
+
 // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 const twoSum = function (array, sum) {
-    
+    for (let i = 0; i < array.length - 1; i++) {
+        for (j = i + 1; j < array.length; j++) {
+            if(array[i] + array[j] === sum) {
+                return true
+            }
+        }
+    }
+    return false
 }
 
 console.log(twoSum([10, 15, 3, 7], 17))                // true
